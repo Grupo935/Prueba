927bbc7a22ff939d74bee6e3acdccb2aef124e3e
anabelennj <anabelen.negrete@hotmail.com>
2017-06-15 09:54:45 -0500

    clases para cargar datos
    se implemento clases para cargar datos de cada uno de los archivos csv,
    cada clase retorna un lista de objetos de cada clase
    soy un papi #daniel

---
+++ ProcesoDeDatos/CargaCliente.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ProcesoDeDatos;
+
+
+import catalogo.Cliente;
+import com.opencsv.CSVReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.LinkedList;
+
+/**
+ *
+ * @author Ana Belen
+ */
+public class CargaCliente {
+    public static LinkedList<Cliente> cargaDatos() throws FileNotFoundException, IOException{
+        LinkedList<Cliente> clientes= new LinkedList<Cliente>();
+        CSVReader reader = new CSVReader(new FileReader("src/CSV/Clientes.csv"));
+        String [] nextLine;
+     while ((nextLine = reader.readNext()) != null) {
+        // nextLine[] is an array of values from the line
+        System.out.println(nextLine[0]);
+        Cliente cliente=new Cliente(nextLine[0],nextLine[1],nextLine[2],nextLine[3],nextLine[4]);
+        clientes.add(cliente);
+     }
+           
+            
+     return clientes;
+    }
+    
+}
